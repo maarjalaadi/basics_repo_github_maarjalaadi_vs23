cfe72d4b739739b51c330222a0062bc62887aff9
Maarja Laadi <maarja.laadi@voco.ee>
2023-09-19 13:52:44 +0300

    datatypes and conversions

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+//data types
+// String
+let firstname = 'Maarja'
+let lastname = 'Laadi'
+console.log(firstname + ' ' + lastname)
+console.log(typeof firstname)
+
+// Numbrid
+let goodMark = 5
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(typeof percent)
+
+// Boolean
+let isToday = true
+console.log(typeof isToday)
+
+// type conversion
+// number to string
+console.log(typeof String(555))
+console.log(String(5+5))
+
+// Boolean to string
+console.log(String(true))
+console.log(Number('66.6'))
+console.log(parseInt(100.3))
+console.log(parseFloat(100.3))
