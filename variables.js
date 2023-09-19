d169cfdec78d2402eaef34cf1b7e34ec4f30f15f
Maarja Laadi <maarja.laadi@voco.ee>
2023-09-19 12:02:53 +0300

    js basics - declare, assign and use

---
+++ variables.js
@@ -0,0 +1,14 @@
+//Mul tuleb juttu muutujatest
+//let
+// keyword variablename = value;
+let programmingLanguage = 'Java Script';
+console.log(programmingLanguage);
+// assignment /save new value to the variable
+programmingLanguage = 'PHP'
+console.log(programmingLanguage)
+let result
+const group = 'VS23'
+// group = 'PG23' - not allowed
+
+console.log(group)
+
