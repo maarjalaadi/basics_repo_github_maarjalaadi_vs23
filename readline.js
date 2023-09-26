428efaf3382fdc7d1d86570d181bcae687364c20
Maarja Laadi <maarja.laadi@voco.ee>
2023-09-26 15:25:00 +0300

    readline.js

--- readline.js
+++ readline.js
@@ -1,11 +1,11 @@
 const readline = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout,
-})
-readline.question('What is your firstname?', firstname
-=> {readline.question('What is your lastname?'), lastname
-=> {
-console.log('Hi' + firstname + lastname);
-readline.close();	
-}
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('What is your firstname? ', firstname => {
+  readline.question('What is your lastname? ', lastname => {
+    console.log('Hi ' + firstname + ' ' + lastname);
+    readline.close();
+  })
 });
